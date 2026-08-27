8c7dfc08d7739817c4e3d4fc4eedb38e770d1ed5
alekseibelyi <beloushchenko@gmail.com>
2026-08-27 17:21:21 +0300

    autotest hw1

--- src/main/java/org/example/NumberChecker.java
+++ src/main/java/org/example/NumberChecker.java
@@ -10,7 +10,7 @@ public class NumberChecker {
         NumberChecker checker = new NumberChecker();
 
         // Тестовые данные
-        int[] testNumbers = {0};
+        int[] testNumbers = {0,1,2,3,4,5,6,7,8,9};
 
         System.out.println("Проверка чисел на четность:");
         System.out.println("=============================");
